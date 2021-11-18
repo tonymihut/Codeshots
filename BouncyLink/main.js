e1ad266b425e574fb9d04dd79b464aaf03855f78
Tony Mihut <tonymihut@gmail.com>
2021-11-18 11:48:50 +0200

    Init

---
+++ BouncyLink/main.js
@@ -0,0 +1,23 @@
+const bouncyLink = document.querySelector(".bouncy-link");
+
+const letters = bouncyLink.textContent.split("");
+
+bouncyLink.textContent = "";
+
+console.log(getComputedStyle(bouncyLink).letterSpacing);
+
+const replaceContent = () =>
+  letters.forEach((letter, index) => {
+    const element = document.createElement("span");
+    element.innerText = letter;
+
+    if (letter === " ") {
+      element.classList.add("spacer");
+    }
+
+    element.style.transitionDelay = `${index * 20}ms`;
+
+    bouncyLink.appendChild(element);
+  });
+
+replaceContent();
--- LivingButton/main.js
+++ LivingButton/main.js
@@ -1,11 +1,10 @@
 const root = document.documentElement;
 const rootStyle = getComputedStyle(root);
-const gradientStart = rootStyle.getPropertyValue("--gradient-start");
-const gradientEnd = rootStyle.getPropertyValue("--gradient-end");
+const gradientStart = rootStyle.getPropertyValue("--purple");
+const gradientEnd = rootStyle.getPropertyValue("--pink");
 const button = document.querySelector(".living-button");
 
 function handleMouseMove(event) {
-  const el = event.currentTarget;
   const cursorPosition = event.offsetX / 2 + -60;
 
   root.style.setProperty(
